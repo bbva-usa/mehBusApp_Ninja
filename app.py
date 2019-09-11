b1e4edf143f62df94c10919c4d938cfe354f02eb
Jeremy Nelson <jcn1990s@gmail.com>
2019-09-11 01:20:23 -0500

    Added more endpoints and test responses from requests.

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from flask import Flask, make_response
 from bson.json_util import dumps
+from objs import Bus
+
 import pymongo
 
 app = Flask(__name__)
+mng_client = pymongo.MongoClient(
+    "mongodb+srv://admin:admin@testcluster0-zvzsm.mongodb.net/test?retryWrites=true&w=majority")
+mng_db = mng_client["BusStops"]  # Replace mongo db name
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.route('/')
+@app.route('/about')
+
+
+def index():
+    return "Hello, Fairfield!"
+
 
 @app.route("/routes")
 def get_routes():
-    mng_client = pymongo.MongoClient("mongodb+srv://admin:admin@testcluster0-zvzsm.mongodb.net/test?retryWrites=true&w=majority")
-    mng_db = mng_client["BusStops"] # Replace mongo db name
+
     collection_name = 'Routes' # Replace mongo db collection name
     db_cm = mng_db[collection_name]
     return make_response(dumps(db_cm.find()))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+@app.route('/buses')
+def getBuses():
+    return "Hello, Fairfield!"
+
+
+@app.route('/buses/<busNumber>')
+def getBus(busNumber):
+    bus = Bus()
+    return json.loads(bus.get_bus())
